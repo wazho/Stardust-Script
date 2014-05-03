1c31f87750efd3adb5195885d897282a029ae59a
Salmon <grass.house@hotmail.com>
2014-05-03 19:43:21 +0800

    [Map Editor] map texture can change for testing I.

--- src/build/material_box.js
+++ src/build/material_box.js
@@ -6,20 +6,20 @@ function MaterialBox() {
 // create container
 MaterialBox.prototype.OnCreate = function() {
 	var that = this ;
-	// material box
+	// Material box.
 	this.box = new createjs.Container() ;
 	this.box.x = 650, this.box.y = 0 ;
 	this.box.bg = new createjs.Shape() ;
 	this.box.bg.graphics.f( "#FFCCAA" ).r( 0, 0, 350, 550 ) ;
 	this.box.addChild( this.box.bg ) ;
-	// material selector container
+	// Material selector container.
 	this.box.selector = new createjs.Container() ;
 	this.box.selector.x = 25, this.box.selector.y = 25 ;
 	this.box.addChild( this.box.selector ) ;
 	this.box.selector.bg = new createjs.Shape() ;
 	this.box.selector.bg.graphics.f( "#FFA54F" ).r( 0, 0, 300, 500 ) ;
 	this.box.selector.addChild( this.box.selector.bg ) ;
-	// material selector button - texture
+	// Material selector button - texture.
 	this.box.selector.buttonA = new createjs.Container() ;
 	this.box.selector.buttonA.x = 0, this.box.selector.buttonA.y = 0 ;
 	this.box.selector.addChild( this.box.selector.buttonA ) ;
@@ -29,7 +29,7 @@ MaterialBox.prototype.OnCreate = function() {
 	this.box.selector.buttonA.text.x = 24, this.box.selector.buttonA.text.y = 7 ;
 	this.box.selector.buttonA.addChild( this.box.selector.buttonA.bg, this.box.selector.buttonA.text ) ;
 	this.box.selector.buttonA.on( "click", function( evt ) { that.OnSelectEffect( that.box.selector.buttonA ) ; that.OnTexture() ; } ) ;
-	// material selector button - walkable
+	// Material selector button - walkable.
 	this.box.selector.buttonB = new createjs.Container() ;
 	this.box.selector.buttonB.x = 100, this.box.selector.buttonB.y = 0 ;
 	this.box.selector.addChild( this.box.selector.buttonB ) ;
@@ -39,7 +39,7 @@ MaterialBox.prototype.OnCreate = function() {
 	this.box.selector.buttonB.text.x = 21, this.box.selector.buttonB.text.y = 7 ;
 	this.box.selector.buttonB.addChild( this.box.selector.buttonB.bg, this.box.selector.buttonB.text ) ;
 	this.box.selector.buttonB.on( "click", function( evt ) { that.OnSelectEffect( that.box.selector.buttonB ) ; that.OnWalkable() ; } ) ;
-	// material selector button - object
+	// Material selector button - object.
 	this.box.selector.buttonC = new createjs.Container() ;
 	this.box.selector.buttonC.x = 200, this.box.selector.buttonC.y = 0 ;
 	this.box.selector.addChild( this.box.selector.buttonC ) ;
@@ -49,7 +49,7 @@ MaterialBox.prototype.OnCreate = function() {
 	this.box.selector.buttonC.text.x = 27, this.box.selector.buttonC.text.y = 7 ;
 	this.box.selector.buttonC.addChild( this.box.selector.buttonC.bg, this.box.selector.buttonC.text ) ;
 	this.box.selector.buttonC.on( "click", function( evt ) { that.OnSelectEffect( that.box.selector.buttonC ) ; that.OnObject() ; } ) ;
-	// material selector button - light
+	// Material selector button - light.
 	this.box.selector.buttonD = new createjs.Container() ;
 	this.box.selector.buttonD.x = 0, this.box.selector.buttonD.y = 25 ;
 	this.box.selector.addChild( this.box.selector.buttonD ) ;
@@ -59,7 +59,7 @@ MaterialBox.prototype.OnCreate = function() {
 	this.box.selector.buttonD.text.x = 35, this.box.selector.buttonD.text.y = 7 ;
 	this.box.selector.buttonD.addChild( this.box.selector.buttonD.bg, this.box.selector.buttonD.text ) ;
 	this.box.selector.buttonD.on( "click", function( evt ) { that.OnSelectEffect( that.box.selector.buttonD ) ; that.OnLight() ; } ) ;
-	// material selector button - sound
+	// Material selector button - sound.
 	this.box.selector.buttonE = new createjs.Container() ;
 	this.box.selector.buttonE.x = 100, this.box.selector.buttonE.y = 25 ;
 	this.box.selector.addChild( this.box.selector.buttonE ) ;
@@ -69,7 +69,7 @@ MaterialBox.prototype.OnCreate = function() {
 	this.box.selector.buttonE.text.x = 31, this.box.selector.buttonE.text.y = 7 ;
 	this.box.selector.buttonE.addChild( this.box.selector.buttonE.bg, this.box.selector.buttonE.text ) ;
 	this.box.selector.buttonE.on( "click", function( evt ) { that.OnSelectEffect( that.box.selector.buttonE ) ; that.OnSound() ; } ) ;
-	// material selector button - save
+	// Material selector button - save.
 	this.box.selector.buttonF = new createjs.Container() ;
 	this.box.selector.buttonF.x = 200, this.box.selector.buttonF.y = 25 ;
 	this.box.selector.addChild( this.box.selector.buttonF ) ;
@@ -79,7 +79,7 @@ MaterialBox.prototype.OnCreate = function() {
 	this.box.selector.buttonF.text.x = 35, this.box.selector.buttonF.text.y = 7 ;
 	this.box.selector.buttonF.addChild( this.box.selector.buttonF.bg, this.box.selector.buttonF.text ) ;
 	this.box.selector.buttonF.on( "click", function( evt ) { that.OnSelectEffect( that.box.selector.buttonF ) ; that.OnSave() ; } ) ;
-	// material list container
+	// Material list container.
 	this.box.list = new createjs.Container() ;
 	this.box.list.x = 30, this.box.list.y = 80 ;
 	this.box.addChild( this.box.list ) ;
@@ -126,8 +126,6 @@ MaterialBox.prototype.OnTexture = function() {
 	// Texture select container.
 	this.box.list.texture = new createjs.Container() ;
 	this.box.list.texture.number = 1 ;
-	// Refresh map texture pics. 
-	Refresh( this, 0 ) ;
 	// Page change.
 	this.box.list.page = new createjs.Container() ;
 	this.box.list.page.x = 35, this.box.list.page.y = 355 ;
@@ -157,18 +155,21 @@ MaterialBox.prototype.OnTexture = function() {
 	this.box.list.marked.addChild( this.box.list.marked.pane ) ;
 	// Add to top container.
 	this.box.list.addChild( this.box.list.bg, this.box.list.texture, this.box.list.page, this.box.list.marked ) ;
+	// Refresh map texture pics. 
+	Refresh( this, 0 ) ;
 	// Refresh canvas.
 	stage.update() ;
 
 	function Refresh( pt, num ) {
 		pt.box.list.texture.removeAllChildren() ;
+		pt.box.list.marked.visible = false ;
 		// Change pic and drawing.
 		pt.box.list.texture.number += ( pt.box.list.texture.number + num > 0 ) ? num : 0 ;
 		pt.box.list.texture.pic = new createjs.Bitmap( src + pt.box.list.texture.number + ".png" ) ;
 		for ( i = 0 ; i < range ; i ++ )
 			for ( j = 0 ; j < range ; j ++ ) {
 				pt.box.list.texture.pic.sourceRect = new createjs.Rectangle( j * size, i * size, size, size ) ;
-				pt.box.list.texture.pic.name = j + 4 * i ;
+				pt.box.list.texture.pic.name = pt.box.list.texture.number * 100 + ( j + 4 * i ) ;
 				pt.box.list.texture.pic.x = 15 + j * ( size + 1 ), pt.box.list.texture.pic.y = 15 + i * ( size + 1 ) ;
 				pt.box.list.texture.addChild( pt.box.list.texture.pic.clone( false ) ) ;
 			} // for
@@ -178,17 +179,20 @@ MaterialBox.prototype.OnTexture = function() {
 		pt.box.list.texture.addChild( pt.box.list.picName ) ;
 		// Add listening event.
 		for ( i = 0 ; i < range * range ; i ++ )
-			pt.box.list.texture.getChildAt( i ).on( "click", function( evt ) { MarkedSelected( that, this.name ) ; } ) ;
+			pt.box.list.texture.getChildAt( i ).on( "click", function( evt ) { MarkedSelected( that, this ) ; } ) ;
 		// Refresh canvas.
 		stage.update() ;
 
-		function MarkedSelected( pt, num ) {
+		// Number of map : Math.floor( tiled.name / 100 )
+		// Index of tiled : tiled.name - Math.floor( tiled.name / 100 ) * 100
+		function MarkedSelected( pt, tiled ) {
 			pt.box.list.marked.visible = true ;
-			pt.box.list.marked.x = 14 + ( num % range ) * ( size + 1 ) ;
-			pt.box.list.marked.y = 14 + ( Math.floor( num / range ) ) * ( size + 1 ) ;
+			index = tiled.name - Math.floor( ( tiled.name / 100 ) ) * 100 ;
+			pt.box.list.marked.x = 14 + ( index % range ) * ( size + 1 ) ;
+			pt.box.list.marked.y = 14 + ( Math.floor( index / range ) ) * ( size + 1 ) ;
 			stage.update() ;
-		} // selected()
-	} // refresh()
+		} // MarkedSelected()
+	} // Refresh()
 } // OnTexture()
 
 // 
--- src/build/preview_box.js
+++ src/build/preview_box.js
@@ -5,16 +5,52 @@ function PreviewBox() {
 
 // create container
 PreviewBox.prototype.OnCreate = function() {
-	// map preview box
+	// Map preview box.
 	this.box = new createjs.Container() ;
 	this.box.x = 0, this.box.y = 0 ;
 	this.box.bg = new createjs.Shape() ;
-	this.box.bg.graphics.beginFill( "#DDFFDD" ).drawRect( 0, 0, 650, 550 ) ;
+	this.box.bg.graphics.f( "#DDFFDD" ).r( 0, 0, 650, 550 ) ;
 	this.box.addChild( this.box.bg ) ;
-	// map box
-	this.box.mapbox = new createjs.Shape() ;
-	this.box.mapbox.x = 50, this.box.mapbox.y = 50 ;
-	this.box.mapbox.graphics.beginFill( "#BBDDAA" ).drawRect( 0, 0, 550, 450 ) ;
+	// Map box.
+	this.box.mapbox = new createjs.Container() ;
+	this.box.mapbox.x = 37, this.box.mapbox.y = 50 ;
+	this.box.mapbox.bg = new createjs.Shape() ;
+	this.box.mapbox.bg.graphics.f( "#BBDDAA" ).r( 0, 0, 576, 448 ) ;
+	this.box.mapbox.addChild( this.box.mapbox.bg ) ;
+	this.OnTiledControl() ;
+
 	this.box.addChild( this.box.mapbox ) ;
-	
 } // OnCreate()
+
+
+PreviewBox.prototype.OnTiledControl = function() {
+	var that = this ;
+	var size = 64, length = 9, height = 7 ;
+	var src = "pic/map/texture/" ;
+	// Map box tiled.
+	this.box.mapbox.tiled = new createjs.Container() ;
+	this.box.mapbox.tiled.x = 0, this.box.mapbox.tiled.y = 0 ;
+	this.box.mapbox.addChild( this.box.mapbox.tiled ) ;
+	this.box.mapbox.tiled.pic = new createjs.Bitmap( src + "0.png" ) ;
+	for ( i = 0 ; i < height ; i ++ )
+		for ( j = 0 ; j < length ; j ++ ) {
+			this.box.mapbox.tiled.pic.sourceRect = new createjs.Rectangle( 0, 0, size, size ) ;
+			this.box.mapbox.tiled.pic.name = i * length + j ;
+			this.box.mapbox.tiled.pic.x = j * size, this.box.mapbox.tiled.pic.y = i * size ;
+			this.box.mapbox.tiled.addChild( this.box.mapbox.tiled.pic.clone( false ) ) ;
+		} // for
+	// Add listening event.
+	for ( i = 0 ; i < length * height ; i ++ )
+		this.box.mapbox.tiled.getChildAt( i ).on( "click", function( evt ) { Refresh( that, this ) ; } ) ;
+
+
+	function Refresh( pt, tiled ) {
+		console.log( tiled.name ) ;
+
+		//tiled = new createjs.Bitmap( src + "1.png" ) ;
+		tiled.sourceRect = new createjs.Rectangle( 64, 64, size, size ) ;
+
+		// Refresh canvas.
+		stage.update() ;
+	} // Refresh()
+} // OnTiledControl()
