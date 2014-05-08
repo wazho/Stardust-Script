b571a6f46c8c47725d9b8064cf63ea7b7aebb07f
Salmon <grass.house@hotmail.com>
2014-05-08 08:48:30 +0800

    Add vendor object.\

--- pic/map/object/adding.js
+++ pic/map/object/adding.js
@@ -15,21 +15,32 @@ var object_adding = [
 		decs : "big house"
 	},
 	{	file : "5.png",
+		decs : "fruit vendor", 
+		divi : 105
+	},
+	{	file : "6.png",
+		decs : "bread vendor", 
+		divi : 105
+	},
+	{	file : "7.png",
 		decs : "arched door", 
 		divi : 170
 	},
-	{	file : "6.png",
+	{	file : "8.png",
 		decs : "guard statue", 
 		divi : 175
 	},
-	{	file : "7.png",
-		decs : "water bucket"
-	},
-	{	file : "8.png",
-		decs : "tree 1"
-	},
 	{	file : "9.png",
-		decs : "tree 2"
+		decs : "water bucket", 
+		divi : 0
+	},
+	{	file : "10.png",
+		decs : "tree 1", 
+		divi : 165
+	},
+	{	file : "11.png",
+		decs : "tree 2", 
+		divi : 165
 	}
 
 ] ;
