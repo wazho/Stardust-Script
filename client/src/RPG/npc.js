c1293035e1cee430613cf050368d8c11400eca5d
Salmon <grass.house@hotmail.com>
2014-05-21 10:44:48 +0800

    Combine the parameter of sheet_sprite.

--- client/src/RPG/npc.js
+++ client/src/RPG/npc.js
@@ -1,13 +1,12 @@
-function NPC( MapControl, Name, x, y, direction, sheet_type, sheet_name ) {
-	this.OnCreate( MapControl, Name, x, y, direction, sheet_type, sheet_name ) ;
+function NPC( MapControl, Name, x, y, sheet, direction ) {
+	this.OnCreate( MapControl, Name, x, y, sheet, direction ) ;
 	return this ;
 } // NPC() 
 
 // NPC character object is created.
-NPC.prototype.OnCreate = function( MapControl, Name, x, y, sheet_type, sheet_name, direction ) {
+NPC.prototype.OnCreate = function( MapControl, Name, x, y, sheet, direction ) {
 	// Magic Number !!
 	this.spriteSize = 125 ;
-	this.effectSpriteSize = 200 ;
 	// Map back and map front are both in map controller.
 	this.MapControlPointer = MapControl ;
 	// NPC container created.
@@ -20,33 +19,33 @@ NPC.prototype.OnCreate = function( MapControl, Name, x, y, sheet_type, sheet_nam
 	this.container.name = Name ;
 	this.container.type = "NPC" ;
 	this.container.direction = direction ;
-	this.container.sheet_type = sheet_type, this.container.sheet_name = sheet_name ;
+	this.container.sheet = sheet ;
 	this.container.grid_x = x, this.container.grid_y = y ;
 	// Adding to the map (in front od all map elements).
 	this.MapControlPointer.container_front.addChild( this.container ) ;
 	// NPC's sprite.
-	this.sprite = new createjs.Sprite( SettingSprite( sheet_type, sheet_name ) ) ;
+	this.sprite = new createjs.Sprite( SettingSprite( sheet.type, sheet.name ) ) ;
 	this.sprite.regX = this.spriteSize / 2, this.sprite.regY = this.spriteSize / 2 ;
 	this.container.addChild( this.sprite ) ;
 	// Default the direction of this NPC.
-	this.OnDirection( this.direction, "front" ) ;
+	this.OnDirection( this.container.direction, "front" ) ;
 	// Shadow created.
 	this.sprite.shadow = new createjs.Shadow( "#454", 5, 5, 5 ) ;
 } // OnCreate()
 
 // Clone the NPC. Except NPC's info isn's inherited, all for the script is inherited.
 NPC.prototype.Clone = function( name, x, y, direction ) {
-	new NPC( this.MapControlPointer, name, x, y, this.container.sheet_type, this.container.sheet_name, direction ) ;
+	new NPC( this.MapControlPointer, name, x, y, this.container.sheet, direction ) ;
 } // Clone()
 
-// 旋轉角色方向/改變播放圖層
+// Rotate and change the sprite sheet.
 NPC.prototype.OnDirection = function( direction, type ) {
 	this.sprite.scaleX = ( direction != -1 ) ? ( ( direction > 0 && direction <= 4 ) ? -1 : 1 ) : direction ;
 	this.direction = ( direction != -1 ) ? direction : this.direction ;
 	this.sprite.gotoAndPlay( type ) ;
 } // OnDirection()
 
-// 角色視窗型對話
+// Open a dialog for player's window.
 NPC.prototype.OnDialog = function() {
 	$( "#dialog_01" ).dialog( "open" ) ;
 } // OnDialog()
