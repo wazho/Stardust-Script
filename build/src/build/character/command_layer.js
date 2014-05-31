7ddb2697ee7d80ed8af5ebebf21531254d6645fe
Salmon <grass.house@hotmail.com>
2014-05-31 11:45:50 +0800

    Add build character - head.

--- build/src/build/character/command_layer.js
+++ build/src/build/character/command_layer.js
@@ -12,18 +12,51 @@ CommandLayer.prototype.OnCreate = function() {
 	this.box.addChild( this.bg ) ;
 
 
-	this.character = new createjs.Container() ;
-	this.character.head = new createjs.Shape() ;
-	this.character.head.graphics.f( "#FFE7CD" )
-	.mt(30,30).qt(30,0,45,0).lt(85,0).qt(100,0,100,30)
-	.lt(100,40).qt(100,50,85,50).lt(40,50).qt(30,47,30,40)
-	.qt(20,30,25,23).qt(33,22,30,30).cp() ;
+	// this.character.head.graphics.f( "#FFE7CD" )
+	// .mt(30,30).qt(30,0,45,0).lt(85,0).qt(100,0,100,30)
+	// .lt(100,40).qt(100,50,85,50).lt(40,50).qt(30,47,30,40)
+	// .qt(20,30,25,23).qt(33,22,30,30).cp() ;
 
-	this.character.addChild( this.character.head ) ;
+
+	this.character = new createjs.Container() ;
+	this.character.body = new createjs.Container() ;
+	this.character.body.head = new createjs.Container() ;
+	this.character.body.head.head = new createjs.Shape() ;
+	this.character.body.head.eyes = new createjs.Shape() ;
+	this.character.body.head.mouth = new createjs.Shape() ;
+
+	this.character.body.head.head.graphics.f("#FFFFFF").s("#000000").dc(30,30,15) ;
+
+	this.character.body.head.eyes.graphics.f("#000000").r(26,26,2,5) ;
+	this.character.body.head.eyes.graphics.f("#000000").r(39,26,2,5) ;
+
+	this.character.body.head.mouth.graphics.s("#000000")
+	.mt(27,37).qt(32,42,39,38) ;
+
+
+	this.character.body.head.addChild( this.character.body.head.head, this.character.body.head.eyes, this.character.body.head.mouth ) ;
+
+
+
+
+
+
+	this.character.body.addChild( this.character.body.head ) ;
+
+	this.character.addChild( this.character.body ) ;
+
+
+
+	createjs.Tween.get( this.character.body.head, { loop: true } ).to( { rotation: 3 }, 500 ).to( { rotation: 0 }, 500 ) ;
 
 
 	this.character.x = 200, this.character.y = 200 ;
 	this.character.scaleX = this.character.scaleY = 2 ;
+
+
+
+
+
 
 	this.box.addChild( this.character ) ;
 
