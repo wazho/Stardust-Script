91d7310891a206ac585c61da82597617d51fe992
Salmon <grass.house@hotmail.com>
2014-06-01 10:40:47 +0800

    Add new characrer build.

--- build/src/build/character/command_layer.js
+++ build/src/build/character/command_layer.js
@@ -11,54 +11,217 @@ CommandLayer.prototype.OnCreate = function() {
 	this.bg.graphics.f( "#115599" ).r( 0, 0, 600, 550 ) ;
 	this.box.addChild( this.bg ) ;
 
+	sheet = Preload() ;
 
-	// this.character.head.graphics.f( "#FFE7CD" )
-	// .mt(30,30).qt(30,0,45,0).lt(85,0).qt(100,0,100,30)
-	// .lt(100,40).qt(100,50,85,50).lt(40,50).qt(30,47,30,40)
-	// .qt(20,30,25,23).qt(33,22,30,30).cp() ;
+	this.box.addChild( Player1() ) ;
+	this.box.addChild( Player3() ) ;
+	this.box.addChild( Player4() ) ;
+	this.box.addChild( Player2() ) ;
 
+	function Player1() {
+		var character = new createjs.Container() ;
 
-	this.character = new createjs.Container() ;
-	this.character.body = new createjs.Container() ;
-	this.character.body.head = new createjs.Container() ;
-	this.character.body.head.head = new createjs.Shape() ;
-	this.character.body.head.eyes = new createjs.Shape() ;
-	this.character.body.head.mouth = new createjs.Shape() ;
+		character.body = new createjs.Container() ;
+		character.body.sprite = new createjs.Sprite( sheet.bodySpriteSheet ) ;
+		character.body.regX = 125 / 2, character.body.regY = 125 ;
+		character.body.x = 0, character.body.y = 125 ;
 
-	this.character.body.head.head.graphics.f("#FFFFFF").s("#000000").dc(30,30,15) ;
+		character.body.addChild( character.body.sprite ) ;
+		character.addChild( character.body ) ;
 
-	this.character.body.head.eyes.graphics.f("#000000").r(26,26,2,5) ;
-	this.character.body.head.eyes.graphics.f("#000000").r(39,26,2,5) ;
+		character.hair = new createjs.Container() ;
+		character.hair.sprite = new createjs.Sprite( sheet.hairSpriteSheet ) ;
+		character.hair.regX = Math.ceil( 75 / 2 ), character.hair.regY = 75 ;
+		character.hair.x = 0, character.hair.y = 60 ;
 
-	this.character.body.head.mouth.graphics.s("#000000")
-	.mt(27,37).qt(32,42,39,38) ;
+		character.hair.addChild( character.hair.sprite ) ;
+		character.addChild( character.hair ) ;
 
+		character.body.sprite.gotoAndPlay( "sit_A" ) ;
+		character.hair.sprite.gotoAndPlay( "stable_B" ) ;
+		character.hair.y += 27 ;
 
-	this.character.body.head.addChild( this.character.body.head.head, this.character.body.head.eyes, this.character.body.head.mouth ) ;
+		character.x = 0, character.y = 0 ;
+		character.scaleX = character.scaleY = 2 ;
 
+		createjs.Tween.get( character.body, { loop: true } ).to( { scaleY: 0.97 }, 1500 ).to( { scaleY: 1 }, 1500 ) ;
+		createjs.Tween.get( character.hair, { loop: true } ).to( { y: character.hair.y + 4 }, 1500 ).to( { y: character.hair.y }, 1500 ) ;
+		createjs.Tween.get( character, { loop: true } ).wait( 1500 )
+		.call( function() { character.body.sprite.gotoAndPlay( ( Math.random() > 0.5 ) ? "sit_A" : "sit_B" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( ( Math.random() > 0.5 ) ? "stable_A" : "stable_B" ) ; } ) ;
 
+		return character ;
+	} // Player1()
 
+	function Player2() {
+		var character = new createjs.Container() ;
 
+		character.body = new createjs.Container() ;
+		character.body.sprite = new createjs.Sprite( sheet.bodySpriteSheet ) ;
+		character.body.regX = 125 / 2, character.body.regY = 125 ;
+		character.body.x = 0, character.body.y = 125 ;
 
+		character.body.addChild( character.body.sprite ) ;
+		character.addChild( character.body ) ;
 
-	this.character.body.addChild( this.character.body.head ) ;
+		character.hair = new createjs.Container() ;
+		character.hair.sprite = new createjs.Sprite( sheet.hairSpriteSheet ) ;
+		character.hair.regX = Math.ceil( 75 / 2 ), character.hair.regY = 75 ;
+		character.hair.x = 0, character.hair.y = 60 ;
 
-	this.character.addChild( this.character.body ) ;
+		character.hair.addChild( character.hair.sprite ) ;
+		character.addChild( character.hair ) ;
 
+		character.body.sprite.gotoAndPlay( "walk_C" ) ;
+		character.hair.sprite.gotoAndPlay( "stable_C" ) ;
 
+		character.x = 700, character.y = 350 ;
+		character.scaleX = character.scaleY = 2 ;
 
-	createjs.Tween.get( this.character.body.head, { loop: true } ).to( { rotation: 3 }, 500 ).to( { rotation: 0 }, 500 ) ;
+		createjs.Tween.get( character.body, { loop: true } ).to( { scaleY: 0.97 }, 1500 ).to( { scaleY: 1 }, 1500 ) ;
+		createjs.Tween.get( character.hair, { loop: true } ).to( { y: character.hair.y + 4 }, 1500 ).to( { y: character.hair.y }, 1500 ) ;
+		createjs.Tween.get( character, { loop: true } ).to( { x: -500 }, 5000 ) ;
 
+		return character ;
+	} // Player2()
 
-	this.character.x = 200, this.character.y = 200 ;
-	this.character.scaleX = this.character.scaleY = 2 ;
+	function Player3() {
+		var character = new createjs.Container() ;
 
+		character.body = new createjs.Container() ;
+		character.body.sprite = new createjs.Sprite( sheet.bodySpriteSheet ) ;
+		character.body.regX = 125 / 2, character.body.regY = 125 ;
+		character.body.x = 0, character.body.y = 125 ;
 
+		character.body.addChild( character.body.sprite ) ;
+		character.addChild( character.body ) ;
 
+		character.hair = new createjs.Container() ;
+		character.hair.sprite = new createjs.Sprite( sheet.hairSpriteSheet ) ;
+		character.hair.regX = Math.ceil( 75 / 2 ), character.hair.regY = 75 ;
+		character.hair.x = 0, character.hair.y = 60 ;
 
+		character.hair.addChild( character.hair.sprite ) ;
+		character.addChild( character.hair ) ;
 
+		character.body.sprite.gotoAndPlay( "walk_C" ) ;
+		character.hair.sprite.gotoAndPlay( "stable_C" ) ;
 
-	this.box.addChild( this.character ) ;
+		character.x = -100, character.y = 0 ;
+		character.scaleX = character.scaleY = 2 ;
 
+		createjs.Tween.get( character.body, { loop: true } ).to( { scaleY: 0.97 }, 1500 ).to( { scaleY: 1 }, 1500 ) ;
+		createjs.Tween.get( character.hair, { loop: true } ).to( { y: character.hair.y + 4 }, 1500 ).to( { y: character.hair.y }, 1500 ) ;
+		createjs.Tween.get( character, { loop: true } )
+		.call( function() { character.body.sprite.gotoAndPlay( "walk_C" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_C" ) ; } )
+		.to( { x: -300 }, 2000 )
+		.call( function() { character.body.sprite.gotoAndPlay( "walk_A" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_A" ) ; } )
+		.to( { y: 150 }, 2000 )
+		.call( function() { character.body.sprite.gotoAndPlay( "walk_C" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_C" ) ; } )
+		.call( function() { character.scaleX *= -1 ; } )
+		.to( { x: -100 }, 2000 )
+		.call( function() { character.body.sprite.gotoAndPlay( "walk_E" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_E" ) ; } )
+		.call( function() { character.scaleX *= -1 ; } )
+		.to( { y: 0 }, 2000 ) ;
+
+		return character ;
+	} // Player3()
+
+	function Player4() {
+		var character = new createjs.Container() ;
+
+		character.body = new createjs.Container() ;
+		character.body.sprite = new createjs.Sprite( sheet.bodySpriteSheet ) ;
+		character.body.regX = 125 / 2, character.body.regY = 125 ;
+		character.body.x = 0, character.body.y = 125 ;
+
+		character.body.addChild( character.body.sprite ) ;
+		character.addChild( character.body ) ;
+
+		character.hair = new createjs.Container() ;
+		character.hair.sprite = new createjs.Sprite( sheet.hairSpriteSheet ) ;
+		character.hair.regX = Math.ceil( 75 / 2 ), character.hair.regY = 75 ;
+		character.hair.x = 0, character.hair.y = 60 ;
+
+		character.hair.addChild( character.hair.sprite ) ;
+		character.addChild( character.hair ) ;
+
+		character.body.sprite.gotoAndPlay( "walk_C" ) ;
+		character.hair.sprite.gotoAndPlay( "stable_C" ) ;
+
+		character.x = 700, character.y = 0 ;
+		character.scaleX = character.scaleY = 2 ;
+
+		createjs.Tween.get( character.body, { loop: true } ).to( { scaleY: 0.97 }, 1500 ).to( { scaleY: 1 }, 1500 ) ;
+		createjs.Tween.get( character.hair, { loop: true } ).to( { y: character.hair.y + 4 }, 1500 ).to( { y: character.hair.y }, 1500 ) ;
+		createjs.Tween.get( character, { loop: true } )
+		.call( function() { character.body.sprite.gotoAndPlay( "walk_B" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_B" ) ; } )
+		.to( { x: 100, y: 200 }, 2000 )
+		.call( function() { character.body.sprite.gotoAndPlay( "stand_A" ) ; } )
+		.wait( 2000 )
+		.call( function() { character.body.sprite.gotoAndPlay( "walk_E" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_E" ) ; } )
+		.to( { y: 0 }, 2000 )
+		.call( function() { character.body.sprite.gotoAndPlay( "stand_E" ) ; } )
+		.wait( 500 )
+		.call( function() { character.body.sprite.gotoAndPlay( "sit_A" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_B" ) ; } )
+		.call( function() { character.hair.sprite.y += 27, character.scaleX *= -1 ; } )
+		.wait( 2000 )
+		.call( function() { character.body.sprite.gotoAndPlay( "stand_A" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_A" ) ; } )
+		.call( function() { character.hair.sprite.y -= 27, character.scaleX *= -1 ; } )
+		.wait( 500 )
+		.call( function() { character.body.sprite.gotoAndPlay( "walk_B" ) ; } )
+		.call( function() { character.hair.sprite.gotoAndPlay( "stable_B" ) ; } )
+		.call( function() { character.scaleX *= -1 ; } )
+		.to( { x: 700, y: 300 }, 2000 )
+		.call( function() { character.scaleX *= -1 ; } ) ;
+
+		return character ;
+	} // Player4()
+
+	function Preload() {
+		var bodySpriteSheet = new createjs.SpriteSheet( {
+			"images": ["pic/character/character_basic.png"],
+			"frames": { "width": 125, "height": 125, "regX": 0, "regY": 0, "count": 62 },
+			"animations": {
+				"stand_A": { "frames": [0], "speed": 1 },
+				"stand_B": { "frames": [1], "speed": 1 },
+				"stand_C": { "frames": [2], "speed": 1 },
+				"stand_D": { "frames": [3], "speed": 1 },
+				"stand_E": { "frames": [4], "speed": 1 },
+				"sit_A": { "frames": [9], "speed": 1 },
+				"sit_B": { "frames": [10], "speed": 1 },
+				"sit_C": { "frames": [11], "speed": 1 },
+				"sit_D": { "frames": [12], "speed": 1 },
+				"sit_E": { "frames": [13], "speed": 1 },
+				"walk_A": { "frames": [18,19,20,21,22,23,24,25], "speed": 0.13 },
+				"walk_B": { "frames": [27,28,29,30,31,32,33,34], "speed": 0.13 },
+				"walk_C": { "frames": [36,37,38,39,40,41,42,43], "speed": 0.13 },
+				"walk_D": { "frames": [45,46,47,48,49,50,51,52], "speed": 0.13 },
+				"walk_E": { "frames": [54,55,56,57,58,59,60,61], "speed": 0.13 }
+			}
+		} ) ;
+		
+		var hairSpriteSheet = new createjs.SpriteSheet( {
+			"images": ["pic/character/character_hair_basic.png"],
+			"frames": { "width": 75, "height": 75, "regX": 0, "regY": 0, "count": 15 },
+			"animations": {
+				"stable_A": { "frames": [0], "speed": 1 },
+				"stable_B": { "frames": [1], "speed": 1 },
+				"stable_C": { "frames": [2], "speed": 1 },
+				"stable_D": { "frames": [3], "speed": 1 },
+				"stable_E": { "frames": [4], "speed": 1 }
+			}
+		} ) ;
+
+		return { "bodySpriteSheet": bodySpriteSheet, "hairSpriteSheet": hairSpriteSheet } ;
+	} // Preload
 
 } // OnCreate()
